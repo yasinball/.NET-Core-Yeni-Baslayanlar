331363736f1d8fcbd208f2b050d70c8ae2c43d8a
yasinball <142742659+yasinball@users.noreply.github.com>
2023-09-20 22:08:25 +0300

    Add files via upload

---
+++ console-programlama/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+Console.WriteLine("İsminizi giriniz");
+string name= Console.ReadLine();
+Console.WriteLine("Soyİsminizi giriniz");
+string surname= Console.ReadLine();
+Console.WriteLine("Merhaba" +  name +  surname);
+
+
+Console.ReadKey();
+
+
+
