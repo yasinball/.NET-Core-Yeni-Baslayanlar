d15c3387bca180c368e6b333724b024d70108b4d
yasinball <142742659+yasinball@users.noreply.github.com>
2023-09-20 22:12:13 +0300

    Add files via upload

---
+++ If-else-else if/Program.cs
@@ -0,0 +1,60 @@
+﻿
+int time=DateTime.Now.Hour;
+if(time>=6 && time < 11)
+{
+    System.Console.WriteLine("Günaydın");
+}
+else if (time<=18)
+{
+    System.Console.WriteLine("İyi Günler");
+}
+else
+{
+    System.Console.WriteLine("İyi geceler");
+}
+
+string sonuc=time<=18 ? "İyi Günler" : "İyi geceler ";
+System.Console.WriteLine(sonuc);
+
+
+string sonucc= time >= 6 && time<11 ? "Günaydın" :time <=18 ? "İyi Günler" : "İyi geceler";
+System.Console.WriteLine(sonucc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+System.Console.ReadLine();
---
+++ Operatörler/Program.cs
@@ -0,0 +1,117 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Atama ve İşlemli Atama
+int x=3;
+int y=3;
+y=y+2;
+System.Console.WriteLine(y);
+
+y+=2;
+System.Console.WriteLine(y);
+
+y/=1;
+System.Console.WriteLine(y);
+
+x*=2;
+System.Console.WriteLine(x);
+
+//Mantıksal Operatörler
+// ||, &&, !
+
+
+// && ve demek
+// || veya 
+// != Eşit Değilse
+bool isSucces=true;
+bool isCompleted=false;
+
+if( isSucces && isCompleted){
+    System.Console.WriteLine("Perfect");
+}
+if( isSucces || isCompleted){
+    System.Console.WriteLine("Perfect");
+}
+if( isSucces != isCompleted){
+    System.Console.WriteLine("Fine");
+}
+
+//İlişkisel Operatörler
+//<,>,<=,>=,==,!=
+
+int a =3;
+int b=4;
+bool sonuc=a>b;
+System.Console.WriteLine(sonuc);
+sonuc=a>b;
+System.Console.WriteLine(sonuc);
+sonuc=a>=b;
+System.Console.WriteLine(sonuc);
+sonuc=a<=b;
+System.Console.WriteLine(sonuc);
+sonuc=a==b;
+System.Console.WriteLine(sonuc);
+sonuc=a!=b;
+System.Console.WriteLine(sonuc);
+
+
+
+// Aritmatik Operatörler
+
+// +,-,/,*
+
+int sayı1=10;
+int sayı2=5;
+
+int sonuc1 =sayı1/sayı2;
+
+System.Console.WriteLine(sonuc1);
+
+sonuc1=sayı1*sayı2;
+System.Console.WriteLine(sonuc1);
+
+
+// % mod alır
+
+int sonuc2=20%3;
+System.Console.WriteLine(sonuc2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
