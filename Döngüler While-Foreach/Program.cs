12129892ec67eb5875d9c47b81eda978e39c25cb
yasinball <142742659+yasinball@users.noreply.github.com>
2023-09-20 22:31:24 +0300

    Add files via upload

---
+++ Döngüler While-Foreach/Program.cs
@@ -0,0 +1,49 @@
+﻿
+//While 
+//1 den baslayarak consoldan girilen sayıya kadar sayı dahil ortalama hesaplayıp konsola yazdıran program
+System.Console.WriteLine("Lütfen Bir Sayı Giriniz");
+int kullanıcı=int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while (sayac <= kullanıcı)
+{
+    toplam+= sayac;
+    sayac ++;
+    System.Console.WriteLine(toplam/kullanıcı);
+}
+
+// a dan z ye kadar tüm harfleri konsola yazdıralım
+
+char character='a';
+
+while (character<='z')
+{
+    System.Console.Write(character);
+    character ++;
+}
+
+
+System.Console.WriteLine("*******FOREACH********");
+string[] arabalar={"BMW","FORD","TOYATA","NİSSAN"};
+foreach (var araba in arabalar)
+{
+     System.Console.WriteLine(araba);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+System.Console.ReadKey();
